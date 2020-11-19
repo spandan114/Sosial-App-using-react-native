5132924447b26799a6ca093d675e5402a3467c46
spandan joshi <spandanj685@gmail.com>
2020-11-20 02:00:11 +0530

    completed

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,15 +1,56 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React,{useEffect} from 'react'
+import { StyleSheet, Text, View,FlatList,SafeAreaView } from 'react-native'
+import {Container,H1} from 'native-base'
+import {connect} from "react-redux";
+import {getPost} from "../action/post"
+import propType from 'prop-types'
+import Post from '../component/Post'
+import EmptyContainer from '../component/EmptyContainer'
 
-const Home = () => {
+const Home = ({GetPost,PostState,userDetails}) => {
+
+  useEffect(()=>{
+    GetPost()
+  },[])
+
+  if(PostState.loding){
+    return <EmptyContainer/>
+  }
+
     return (
-        <View>
-            <Text></Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <FlatList
+            data={PostState.posts}
+            keyExtractor = {(item) => item.id}
+            renderItem = {({item,index,separators})=>(
+              <Post item={item} key={index} userDetails={userDetails} />
+            )}
+            ListEmptyComponent={() => (
+              <Container style={styles.emptyContainer}>
+                <H1>No post fond</H1>
+              </Container>
+            )}
+            ></FlatList>
+        </SafeAreaView>
     )
 }
 
-export default Home
+const mapDispatchToProps = {
+  GetPost: () => getPost(),
+}
+
+const mapStateToProps = (state) => ({
+  PostState: state.post,
+  userDetails: state.auth.user
+})
+
+Home.propTypes = {
+  GetPost: propType.func.isRequired,
+  PostState: propType.object.isRequired,
+  userDetails: propType.object
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Home)
 
 const styles = StyleSheet.create({
     container: {
